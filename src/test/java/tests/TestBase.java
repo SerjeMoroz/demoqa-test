5f146f2e250166688c035279075ce950fa62860d
Sergei Moroz <moroz.sergei94@gmail.com>
2024-10-31 21:49:17 +0300

    добавлен скролл

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -11,8 +11,10 @@ public class TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
+
     @BeforeAll
     static void beforeAll() {
+        Configuration.fastSetValue = true;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
     }
