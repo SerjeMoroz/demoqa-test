7ae45179928572c79bc5ff7d24e45fc2add21b60
Sergei Moroz <moroz.sergei94@gmail.com>
2025-02-26 23:08:19 +0300

    add constants

--- src/test/java/examples/Constants.java
+++ src/test/java/examples/Constants.java
@@ -3,4 +3,6 @@ package examples;
 public class Constants {
     public static final String TITLE_TEXT = "Student Registration Form";
     public static final String REGISTRATION_FORM_TEXT = "Thanks for submitting the form";
+    public static final String[] HOBBIE = {"Sports" , "Music", "Reading"};
+    public static final String[] GENDER = {"Male", "Female", "Other"};
 }
--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -4,23 +4,21 @@ import examples.Faker;
 import examples.TestBase;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
-
 import static pages.components.TestData.*;
 
 public class RegistrationWithPageObjectsTests extends TestBase {
-
     private final RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
     void registrationTest() {
-        String userName = Faker.firstName();
+        String firstName = Faker.firstName();
         String lastName = Faker.lastName();
         String email = Faker.email();
         String phone = Faker.phone();
         String address = Faker.address();
 
         registrationPage.openPage()
-                .setFirstName(userName)
+                .setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(email)
                 .setGender(gender)
@@ -33,9 +31,8 @@ public class RegistrationWithPageObjectsTests extends TestBase {
                 .setCity(city)
                 .submitButton();
 
-
-                registrationPage.verifyResultsModalAppears()
-                .verifyResult("Student Name", userName + " " + lastName)
+        registrationPage.verifyResultsModalAppears()
+                .verifyResult("Student Name", firstName + " " + lastName)
                 .verifyResult("Student Email", email)
                 .verifyResult("Gender", gender)
                 .verifyResult("Mobile", phone)
